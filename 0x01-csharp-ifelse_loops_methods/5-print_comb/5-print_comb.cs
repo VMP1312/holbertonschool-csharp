5cba5a30ca44faf3f4530e1623df354a4d8446b3
VMP1312 <victor.paz1312@hotmail.com>
2020-12-01 02:18:02 -0500

    First build

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            for (num = 0; num < 99; num++) {
+                Console.Write("{0:D2}, ", num);
+            }
+            Console.Write("99\n");
+        }
+    }
+}
