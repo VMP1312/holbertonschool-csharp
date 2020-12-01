e193ed4b45c2c9c6cc284e3f28193309b304582b
VMP1312 <victor.paz1312@hotmail.com>
2020-12-01 01:52:42 -0500

    First build

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            for (c = 'a'; c <= 'z'; c++) 
+            {
+                if (c == 'e' || c == 'q')
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Write("{0}", c);
+                }
+            }
+        }
+    }
+}
