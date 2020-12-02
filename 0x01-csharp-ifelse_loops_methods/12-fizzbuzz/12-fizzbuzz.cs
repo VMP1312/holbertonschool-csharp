3683e52b7a7977d25f083eeb8758c0e95152010c
VMP1312 <victor.paz1312@hotmail.com>
2020-12-01 19:59:08 -0500

    First build

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt;
+            for (cnt = 1; cnt <= 99; cnt++) {
+                if (cnt % 3 == 0 && cnt % 5 == 0) 
+                    Console.Write("FizzBuzz ");
+
+                else if (cnt % 3 == 0)
+                    Console.Write("Fizz ");
+
+                else if (cnt % 5 == 0)
+                    Console.Write("Buzz ");
+
+                else
+                    Console.Write("{0} ", cnt);
+            }
+            Console.WriteLine("Buzz");
+        }
+    }
+}
