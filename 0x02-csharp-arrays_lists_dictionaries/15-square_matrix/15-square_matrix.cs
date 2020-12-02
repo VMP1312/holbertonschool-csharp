c6cfcf455f8df1a4180c61fbbe51aa406a501c83
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 05:08:23 -0500

    First build

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int width = myMatrix.GetLength(0);
+        int height = myMatrix.GetLength(1);
+        int[,] NMatrix = new int[width,height];
+        double square = 0;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                square = Math.Pow(myMatrix[x, y], 2);
+                NMatrix[x, y] = (int)square;
+            }
+        } 
+        return NMatrix;
+    }
+}
