51aada92b6371d370a594dac3f71f31e82b99839
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 01:09:58 -0500

    First build

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        int cnt;
+        if (array == null || array.Length == 0)
+            Console.WriteLine();
+
+        else
+        {
+            cnt = array.Length - 1;
+
+            for (cnt; cnt >= 0; cnt--)
+            {
+                if (cnt <= 1)
+                    Console.WriteLine(array[cnt]);
+
+                else
+                    Console.Write(array[cnt] + " ");
+
+            }
+        }
+    }
+}
