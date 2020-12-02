08b85201836783e0c7a92ca2e817d16d51864af2
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 02:35:07 -0500

    First build

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string name = "0";
+        if(myList.Count == 0)
+            return "None";
+
+
+        else
+        {
+        foreach (KeyValuePair<string, int> elem in myList.OrderByDescending(key => key.Value))
+            {
+                    name = elem.Key;
+                    return(name);
+            }
+        }
+        return(name);
+    }
+}
