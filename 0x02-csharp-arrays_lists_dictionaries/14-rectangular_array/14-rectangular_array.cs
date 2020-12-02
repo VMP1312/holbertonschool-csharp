4751ca12a4d80380089562e253c9fa67424da6d5
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 04:48:15 -0500

    First Build

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[5, 5];
+        array[2,2] = 1;
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                if (y < 4)
+                    Console.Write(array[x,y] + " ");
+
+                else
+                    Console.Write(array[x,y]);
+            }
+        Console.WriteLine();
+        }
+    }  
+}
