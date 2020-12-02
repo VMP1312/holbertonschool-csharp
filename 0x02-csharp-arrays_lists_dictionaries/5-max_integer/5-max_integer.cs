85b009fb437c2da71708047cdabc3c80608b84ae
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 01:49:09 -0500

    First build

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int cnt = 1;
+        int max = myList[0];
+
+        while (cnt < myList.Count)
+        {
+            if (myList[cnt] > max)
+                max = myList[cnt];
+
+            cnt++;
+        }
+        return max;
+    } 
+}
