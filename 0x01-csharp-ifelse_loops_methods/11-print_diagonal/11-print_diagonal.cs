8419a474dd34b6c07524b3322419c35c5eae0f00
VMP1312 <victor.paz1312@hotmail.com>
2020-12-01 19:56:02 -0500

    First build

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int i;
+        int j;
+
+        if (length <= 0)
+            Console.Write("\n");
+
+        else
+        {
+            for (i = 1; i <= length; i++)
+            {
+                
+                for (j = 0; j < i; j++)
+                    Console.Write(" ");
+  
+                Console.WriteLine("\\");   
+            }
+            Console.WriteLine("");
+        }
+    }
+}
