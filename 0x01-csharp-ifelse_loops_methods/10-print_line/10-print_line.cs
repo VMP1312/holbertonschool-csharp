e3074db5be7e9f967d9e6c7725dc9ed74c469934
VMP1312 <victor.paz1312@hotmail.com>
2020-12-01 19:52:00 -0500

    First build

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int cnt;
+        if (length <= 0)
+            Console.Write("\n");
+        else
+        {
+            for (cnt = 0; cnt <= length; cnt++)
+                Console.Write("_");
+            Console.WriteLine("");
+        }
+    }
+}
