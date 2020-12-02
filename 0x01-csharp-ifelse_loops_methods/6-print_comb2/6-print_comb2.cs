3cb8ab335ac430344816e2f9f18a8cbc59c7d9d6
VMP1312 <victor.paz1312@hotmail.com>
2020-12-01 19:24:57 -0500

    First build

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+        {
+            int i;
+            int j;
+
+            for (i = 0; i < 8; i++)
+            {
+                for (j = 1; j <= 9; j++)
+                {
+                    if (i < j)
+                    {
+                        Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+            }
+            Console.WriteLine("89");
+        }
+        }
+    }
+}
