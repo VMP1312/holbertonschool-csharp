6d20282da3d02d712e533ca94f6e0e7f44e84bc7
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 00:46:46 -0500

    First build

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int i;
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] array = new int[size];
+
+        if (size == 0)
+            Console.WriteLine();
+
+        for (i = 0; i < size; i++)
+        {
+            array[i] = i;
+
+            if (i < size - 1)
+                Console.Write(array[i] + " ");
+
+            else
+                Console.WriteLine(array[i]);
+        }
+        return array;
+    }
+}
