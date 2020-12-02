c33e93aa6468e2e7f4fa867139bc1b053af78acd
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 02:06:17 -0500

    First issue

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (!myDict.ContainsKey(key))
+        {
+            myDict.Add(key, value);
+            return myDict;
+        }
+        else
+        {
+            myDict[key] = value;
+            return myDict;
+        }
+    }
+}
