08df00c44ecf582de3b7e1e52ae5349c708a80df
VMP1312 <victor.paz1312@hotmail.com>
2020-12-02 01:38:57 -0500

    First build

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int cnt;
+        List<int> Nlist = new List<int>();
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        if (size == 0)
+            Console.WriteLine();
+
+        for (cnt = 0; cnt < size; cnt++)
+        {
+            Nlist.Add(cnt);
+
+            if (cnt < size - 1)
+                Console.Write(Nlist[cnt] + " ");
+
+            else
+                Console.WriteLine(Nlist[cnt]);
+        }
+        return Nlist;
+    }
+}
