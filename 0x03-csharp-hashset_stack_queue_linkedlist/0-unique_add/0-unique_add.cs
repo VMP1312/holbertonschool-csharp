b8b188a5e60ecb39f9bfdd4b29f90fd7c531f73c
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 01:32:07 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> newList = new List<int>();
+        int add = 0;
+        int cnt = 0;
+
+        foreach (int n in myList)
+        {
+            if (newList.Contains(n) == false)
+                newList.Add(n);
+        }
+        for (; cnt < newList.Count; cnt++)
+        {
+            add += newList[cnt];
+        }
+        return add;
+    }
+}
