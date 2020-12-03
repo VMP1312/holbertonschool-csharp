15da0019f8f0d8bb34bdcae183a45cb0ab170a33
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 02:02:44 -0500

    First issue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> NList = new List<int>();
+        foreach (int n in list1)
+        {
+            if (list2.Contains(n))
+                NList.Add(n);
+        }
+        NList.Sort();
+        return NList;
+    }
+}
