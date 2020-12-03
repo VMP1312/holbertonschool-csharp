0346f64b6394bdcd0d8f9d5f2fe919c70b365be9
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 04:11:49 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+        {
+            LinkedListNode<int> mv = myLList.First;
+            while(mv != null)
+            {
+                if(mv.Value < n)
+                    mv = mv.Next;
+
+                else
+                    return(myLList.AddBefore(mv, n));
+
+            }
+            return(myLList.AddLast(n));  
+        }
+    }
