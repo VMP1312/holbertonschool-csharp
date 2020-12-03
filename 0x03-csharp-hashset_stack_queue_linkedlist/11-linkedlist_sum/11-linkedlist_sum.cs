09ed38171e19a25e6486410e1f07d19e6ca7dcb0
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 04:02:42 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int suma = 0;
+        foreach (int n in myLList)
+            suma += n; 
+
+        return suma;
+    }
+}
