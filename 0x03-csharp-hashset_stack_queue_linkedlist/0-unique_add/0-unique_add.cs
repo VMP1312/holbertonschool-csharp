fee34ce8cc8a233496729483755b1d392dcbf8ca
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 01:54:17 -0500

    Fix problems

--- 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -5,19 +5,19 @@ class List
 {
     public static int Sum(List<int> myList)
     {
-        List<int> newList = new List<int>();
+        List<int> NList = new List<int>();
         int add = 0;
         int cnt = 0;
 
         foreach (int n in myList)
         {
-            if (newList.Contains(n) == false)
-                newList.Add(n);
+            if (NList.Contains(n) == false)
+                NList.Add(n);
         }
-        for (; cnt < newList.Count; cnt++)
-        {
-            add += newList[cnt];
-        }
+
+        for (; cnt < NList.Count; cnt++)
+            add += NList[cnt];
+
         return add;
     }
 }
