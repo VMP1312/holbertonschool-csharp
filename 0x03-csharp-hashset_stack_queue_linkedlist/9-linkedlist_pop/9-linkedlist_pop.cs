b22d1b3be407c163c095ee4813fea8c637ed9567
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 03:53:23 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count <= 0)
+            return 0;
+         
+        LinkedListNode<int> first = new LinkedListNode<int>(myLList.First.Value);
+        myLList.RemoveFirst();
+        return first.Value; 
+    }
+}
