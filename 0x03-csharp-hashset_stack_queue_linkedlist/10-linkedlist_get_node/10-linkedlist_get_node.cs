2ceb2ba77fcb57c02316dba3e11f11652ee54ccf
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 03:57:26 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int cnt = 0;
+
+        foreach (int value in myLList)
+        {
+            if (cnt > myLList.Count)
+                return 0;
+
+            else
+            {
+                if (cnt == n)
+                    return value;
+            }
+
+            cnt++;
+        }
+        return 0;
+    }
+}
