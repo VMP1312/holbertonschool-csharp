cd4f68dc2c38b21aaf830fb8becbd97b288438c1
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 16:40:15 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (myLList.Contains(value) == false)
+            return -1;
+
+        int index = 0;
+
+        foreach (int cnt in myLList)
+        {
+            if (cnt == value)
+                return index;
+
+            index++;
+        }
+        return -1;
+    }
+}
