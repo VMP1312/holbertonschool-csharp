a07415de0e7c3559c535ad0109e8b7d09d28b789
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 04:15:25 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int cnt = 0;
+        LinkedListNode<int> mv;
+        mv = myLList.First;
+
+        while (mv != null)
+        {
+            if (cnt == index)
+            {
+                myLList.Remove(mv);
+                break;
+            }
+            cnt++;
+            mv = mv.Next;
+        }
+    }
+}
