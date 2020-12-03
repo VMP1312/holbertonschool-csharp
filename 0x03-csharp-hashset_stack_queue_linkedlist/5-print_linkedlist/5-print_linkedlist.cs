01fc94f833a6bbac9191328a897f7fb5fed927f0
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 03:17:54 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+            LinkedList<int> NList = new LinkedList<int>();
+
+        else
+        {
+            int[] n = new int[size];
+            for (int cnt = 0; cnt < size; cnt++)
+            {
+                n[cnt] = cnt;
+                Console.WriteLine(n[cnt]);
+            }
+            LinkedList<int> NList = new LinkedList<int>(n);        
+        }
+        return NList;
+    }
+}
