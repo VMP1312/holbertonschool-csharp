a540453a0b145f14773ec961b0cb848a0841ef43
VMP1312 <victor.paz1312@hotmail.com>
2020-12-03 03:23:57 -0500

    First build

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myList)
+    {
+        int cnt = 0;
+        foreach (int index in myList)
+            cnt++;
+
+        return cnt;
+    }
+}
