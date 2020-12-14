2a25e62d021c025ea3b4c1f85c10352e214084f1
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2020-12-14 15:23:41 -0500

    First Build

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int cnt = 0;
+        try 
+        {
+            for (int mv = 0; mv < n; mv++)
+            {
+                Console.WriteLine(myList[mv]);
+                cnt++;
+            }
+        }
+        catch
+        {
+
+        }
+        return cnt;
+    }
+}
