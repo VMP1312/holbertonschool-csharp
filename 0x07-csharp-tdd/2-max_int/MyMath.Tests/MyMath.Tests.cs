76bcc8b4d7f28c78097baeaa3b4b22cd958ef129
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2020-12-17 01:00:29 -0500

    UPLOAD

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,41 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace Tests
+namespace MyMath.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class Operations_Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void Empty()
         {
+            List<int> nums = new List<int>();
+
+            int maxi = Operations.Max(nums);
+            Assert.AreEqual(0, maxi);
+        }
+        
+        [Test]
+        public void Null()
+        {
+            List<int> nums = null;
+
+            int maxi = Operations.Max(nums);
+            Assert.AreEqual(0, maxi);
         }
 
         [Test]
-        public void Test1()
+        public void Maxi()
         {
-            Assert.Pass();
+            List<int> nums = new List<int>();
+            nums.Add(1);
+            nums.Add(2);
+            nums.Add(-1);
+            nums.Add(-2);
+
+            int maxi = Operations.Max(nums);
+            Assert.AreEqual(2, maxi);
         }
     }
 }
--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary> Operations</summary>
+
+    public class Operations
     {
+        /// <summary> Max int</summary>
+        public static int Max(List<int> nums)
+        {
+
+            if (nums == null || nums.Count <= 0)
+                return 0;
+
+            int cnt = 1;
+            int maxi = nums[0];
+            while (cnt < nums.Count) 
+            {
+                if (nums[cnt] > maxi) 
+                {
+                    maxi = nums[cnt];
+                }
+                cnt++;
+            }
+            return maxi;
+        }
     }
 }
