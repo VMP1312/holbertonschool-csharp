ac2a6453ad5e492f2e34438beed53601018095e3
VMP1312 <victor.paz1312@hotmail.com>
2021-01-11 11:57:17 -0500

    UPLOAD

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>Lenght of a vector</summary>
+class VectorMath
+{
+    /// <summary>Lenght of a vector</summary>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length >= 2 && vector.Length <= 3)
+        {
+            double acum = 0;
+
+            double lenVec = new double();
+
+            foreach (double x in vector)
+            {
+                double sqrt = Math.Pow(x, 2);
+                acum = acum + sqrt;
+            }
+
+            lenVec = Math.Sqrt(res);
+            return Math.Round(lenVec, 2);
+        }
+
+        return -1;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>Adds two vectors</summary>
+class VectorMath
+{
+    /// <summary>Adds two vectors</summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] err = {-1};
+
+        if (vector1.Length < 2 || vector2.Length < 2 || vector1.Length > 3 || vector2.Length > 3)
+            return err;
+
+        if (vector1.Length != vector2.Length)
+            return err;
+
+        double[] sumVec = new double[vector1.Length];
+        
+        for (int mv = 0; mv < vector1.Length; mv++)
+            sumVec[mv] = vector1[mv] + vector2[mv];
+
+        return sumVec;
+    }
+}
