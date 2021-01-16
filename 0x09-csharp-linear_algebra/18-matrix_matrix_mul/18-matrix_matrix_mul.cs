3af213e97d3769aae878c9f5f7ba2e89c104a4d2
VMP1312 <victor.paz1312@hotmail.com>
2021-01-16 12:37:29 -0500

    UPLOAD

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -6,29 +6,27 @@ class MatrixMath
     /// <summary>Multiplies two matrices and returns the resulting matrix</summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        double[,] notPossible = {{-1}};
+        double[,] err = {{-1}};
+    
         if (matrix1.GetLength(1) != matrix2.GetLength(0))
-        {
-            return notPossible;
-        }
+            return err;
 
         int rows1 = matrix1.GetLength(0);
         int cols1 = matrix1.GetLength(1);
         int cols2 = matrix2.GetLength(1);
-        double[,] newMatrix = new double[rows1, cols2];
+        double[,] NMatrix = new double[rows1, cols2];
 
         for (int x = 0; x < rows1; x++)
         {
             for (int y = 0; y < cols2; y++)
             {
-                double n = 0; 
-                for (int z = 0; z < cols1; z++)
-                {
-                    n = n + matrix1[x, z] * matrix2[z, y];
-                }
-                newMatrix[x, y] = n;
+                double num = 0; 
+                for (int pv = 0; z < cols1; pv++)
+                    num = num + matrix1[x, pv] * matrix2[pv, y];
+
+                NMatrix[x, y] = num;
             }
         }
-        return newMatrix;
+        return NMatrix;
     }
 }
