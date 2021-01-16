d3d7a94d633e981f8155e40a53fb25d5ca469243
VMP1312 <victor.paz1312@hotmail.com>
2021-01-16 12:16:55 -0500

    UPLOAD

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -7,17 +7,15 @@ class MatrixMath
     /// <summary>Multiplies a matrix and a scalar and returns the resulting matrix</summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        double[,] err = { { -1 } };
-        if (matrix.GetLength(0) >= 2 && matrix.GetLength(0) <= 3)
-        {
-            for (int x = 0; x < matrix.GetLength(0); x++)
-            {
-                for (int y = 0; y < matrix.GetLength(1); y++)
-                    matrix[x, y] = matrix[x, y] * scalar;
-            }
-            return matrix;
-        }
+        double[,] err = {{-1}};
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            return err;
 
-        return err;
+        for (int x = 0; x < matrix.GetLength(0); x++)
+        {
+            for (int y = 0; y < matrix.GetLength(1); y++)
+                matrix[x, y] = matrix[x, y] * scalar;
+        }
+        return matrix;
     }
 }
