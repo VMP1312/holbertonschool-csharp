5311170b5bcea3cc3287404a37aff53d1e061e58
VMP1312 <victor.paz1312@hotmail.com>
2021-01-16 13:21:11 -0500

    UPLOAD

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,52 +1,53 @@
 ﻿using System;
 
-/// <summary>Rotate Matrix</summary>
+/// <summary>Matrix</summary>
 class MatrixMath
 {
-    /// <summary>Multiply matrix</summary>
-    public static double[,] Multi(double[,] matrix1, double[,] matrix2)
+    /// <summary>Multiplies two matrices and returns the resulting matrix</summary>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         double[,] err = {{-1}};
     
         if (matrix1.GetLength(1) != matrix2.GetLength(0))
             return err;
 
-        int rws1 = matrix1.GetLength(0);
-        int cls1 = matrix1.GetLength(1);
-        int cls2 = matrix2.GetLength(1);
-        double[,] newMatrix = new double[rws1, cols2];
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int cols2 = matrix2.GetLength(1);
+        double[,] NMatrix = new double[rows1, cols2];
 
-        for (int x = 0; x < rws1; x++)
+        for (int x = 0; x < rows1; x++)
         {
-            for (int y = 0; y < cls2; y++)
+            for (int y = 0; y < cols2; y++)
             {
                 double n = 0; 
-                for (int z = 0; z < cls1; z++)
-                    n = n + Math.Round(matrix1[x, z] * matrix2[z, y]);
-
-                newMatrix[x, y] = n;
+                for (int z = 0; z < cols1; z++)
+                    n = n + matrix1[x, z] * matrix2[z, y];
+                NMatrix[x, y] = n;
             }
         }
-        return newMatrix;
+        return NMatrix;
     }
+
     
-    /// <summary>Rotate Matrix</summary>
+    /// <summary>Rotate</summary>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        double Cos = Math.Cos(angle);
-        double Sin = Math.Sin(angle);
-        double[,] cosSin = 
-            {
-                {Cos, -Sin}, 
-                {Sin, Cos}
+        double ACos = Math.Cos(angle);
+        double ASin = Math.Sin(angle);
+        double[,] cosSin = {
+            {ACos, -ASin}, 
+            {ASin, ACos}
             };
+
         double[,] newMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
-        double[,] err = {{-1}};
+        double[,] notPossible = {{-1}};
 
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-            return err;
-
-        newMatrix = Multi(cosSin, matrix);
+        {
+            return notPossible;
+        }
+        newMatrix = Multiply(cosSin, matrix);
         return newMatrix;
     }
 }
