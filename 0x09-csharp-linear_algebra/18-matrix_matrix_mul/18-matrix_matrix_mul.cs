49a4cd5eb5db4f23e4fb40f8c29f2cd139922e5a
VMP1312 <victor.paz1312@hotmail.com>
2021-01-16 12:24:37 -0500

    UPLOAD

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -10,10 +10,10 @@ class MatrixMath
         if (matrix1.GetLength(1) != matrix2.GetLength(0))
             return err;
 
-        double[,] NMatrix = new double[rows, col2];
         int rows = matrix1.GetLength(0);
         int col1 = matrix1.GetLength(1);
         int col2 = matrix2.GetLength(1);
+        double[,] NMatrix = new double[rows, col2];
 
         for (int x = 0; x < rows; x++)
         {
