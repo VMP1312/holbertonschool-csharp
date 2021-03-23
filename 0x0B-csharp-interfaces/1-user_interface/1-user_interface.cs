2ca5c12a9f84e1fef022d8a115f2b8b781021df1
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-03-23 18:06:12 -0500

    Fix

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -3,6 +3,7 @@
 /// <summary>Base Class</summary>
 public abstract class Base 
 {
+    ///<summary>Public name</summary>
     public string name;
     
     /// <summary>Overides to string method</summary>
@@ -16,35 +17,45 @@ public abstract class Base
 /// <summary>Interface</summary>
 public interface IInteractive
 {
+    ///<summary>Method</summary>
     void Interact();
 }
 
 /// <summary>Interface</summary>
 public interface IBreakable
 {
+    ///<summary>Property</summary>
     int durability
     { get; set; }
+    ///<summary>Method</summary>
     void Break();
 }
 
 /// <summary>Interface</summary>
 public interface ICollectible
 {
+    ///<summary>Property</summary>
     bool isCollected
     { get; set; }
+    ///<summary>Method</summary>
     void Collect();
 }
 
 public class TestObject : Base, IInteractive, IBreakable, ICollectible
 {
+    ///<summary>Property</summary>
     public int durability
     { get; set; }
+    ///<summary>Property</summary>
 	public bool isCollected
     { get; set; }
+    ///<summary>Method</summary>
     public void Interact()
     {}
+    ///<summary>Method</summary>
     public void Break()
     {}
+    ///<summary>Method</summary>
     public void Collect()
     {}
 }
