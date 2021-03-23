c04c832558793661a43c61c7a68c54daecc207e8
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-03-23 18:21:47 -0500

    Fix

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -3,6 +3,7 @@
 /// <summary>Base Class</summary>
 public abstract class Base 
 {
+    ///<summary>Public name</summary>
     public string name;
     
     /// <summary>Overides to string method</summary>
@@ -16,33 +17,39 @@ public abstract class Base
 /// <summary>Interface</summary>
 public interface IInteractive
 {
+    ///<summary>Method</summary>
     void Interact();
 }
 
 /// <summary>Interface</summary>
 public interface IBreakable
 {
+    ///<summary>Property</summary>
     int durability
     { get; set; }
+    ///<summary>Method</summary>
     void Break();
 }
 
 /// <summary>Interface</summary>
 public interface ICollectible
 {
+    ///<summary>Property</summary>
     bool isCollected
     { get; set; }
+    ///<summary>Method</summary>
     void Collect();
 }
 
-/// <summary>Door implementation</summary>
+///<summary>Door implementation</summary>
 public class Door : Base, IInteractive
 {
+    ///<summary>Door constructor</summary>
     public Door(string door = "Door")
     {
         name = door;
     }
-
+    ///<summary>Method</summary>
     public void Interact()
     {
         Console.WriteLine("You try to open the " + name +". It's locked.");
