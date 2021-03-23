b962a02bdfe6e661afb5774eb0cc6df0e57d2985
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-03-23 18:23:40 -0500

    Fix

--- 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
-///<summary>Base Class</summary>
+/// <summary>Base Class</summary>
 public abstract class Base 
 {
+    ///<summary>Public name</summary>
     public string name;
     
-    ///<summary>Overides to string method</summary>
+    /// <summary>Overides to string method</summary>
         public override string ToString()
         {
             return name + " is a " + GetType();
@@ -13,46 +14,55 @@ public abstract class Base
 }
 
 
-///<summary>Interface</summary>
+/// <summary>Interface</summary>
 public interface IInteractive
 {
+    ///<summary>Method</summary>
     void Interact();
 }
 
-///<summary>Interface</summary>
+/// <summary>Interface</summary>
 public interface IBreakable
 {
+    ///<summary>Property</summary>
     int durability
     { get; set; }
+    ///<summary>Method</summary>
     void Break();
 }
 
-///<summary>Interface</summary>
+/// <summary>Interface</summary>
 public interface ICollectible
 {
+    ///<summary>Property</summary>
     bool isCollected
     { get; set; }
+    ///<summary>Method</summary>
     void Collect();
 }
 
 ///<summary>Door implementation</summary>
 public class Door : Base, IInteractive
 {
+    ///<summary>Door constructor</summary>
     public Door(string door = "Door")
     {
         name = door;
     }
-
+    ///<summary>Method</summary>
     public void Interact()
     {
         Console.WriteLine("You try to open the " + name +". It's locked.");
     }
 }
 
+
 ///<summary>The Decoration</summary>
 public class Decoration : Base, IInteractive, IBreakable
 {
+    ///<summary>Property</summary>
     public bool isQuestItem;
+    ///<summary>Property</summary>
     public int durability
     { get; set; }
 
