f5ea78402c5d3e909a1c8b523dc1c2efa012ef98
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-03-23 18:15:51 -0500

    Fix

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -4,7 +4,8 @@
 public abstract class Base 
 {
     ///<summary>Public name</summary>
-    public string name;
+    private string name
+    {get; set;}
     
     /// <summary>Overides to string method</summary>
         public override string ToString()
